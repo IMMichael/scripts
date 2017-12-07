3e15b3d1549516579d8a0f56781565758170a056
Michael <mic613@gmail.com>
2017-12-07 12:06:41 +0200

    files for dummy files generation

--- loopdel.sh
+++ loopdel.sh
@@ -1,7 +1,7 @@
 #/bin/bash
-files="$(du ~/temp/* |sort -rn|awk '{ print $2 }'|xargs)"
-echo "$files"
+files="$(du ~/temp/* |awk '{ print $2 }')"
 for i in "${files[@]}"
 do
 	rm -f $i
 done
+printf "Deleted:\n$files\n"
